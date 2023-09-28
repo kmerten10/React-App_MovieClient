e29eb0474cc77a1604db551ffc5632086c864537
kmerten10 <kelsey.merten@gmail.com>
2023-09-27 20:30:51 -0500

    added api/proptypes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,47 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MovieCard } from "./movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-    const [movie, setMovie] = useState([
-        {
-            id: 1,
-            title: "Mrs. Doubtfire",
-            image:
-                "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
-            director: "Someone"
-        },
-        {
-            id: 2,
-            title: "The Matrix",
-            image:
-                "https://images-na.ssl-images-amazon.com/images/I/51WAikRq37L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-            director: "Someone Else"
-        },
-        {
-            id: 3,
-            title: "Peter Rabbit",
-            image:
-                "https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX381_BO1,204,203,200_.jpg",
-            director: "Peter Rabbit Director"
-        },
-        {
-            id: 4,
-            title: "This is a Movie",
-            image:
-                "https://images-na.ssl-images-amazon.com/images/I/51HbNW6RzhL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-            director: "This is a movie director"
-        },
-        {
-            id: 5,
-            title: "New Movie!",
-            image:
-                "https://images-na.ssl-images-amazon.com/images/I/41MBLi5a4jL._SX384_BO1,204,203,200_.jpg",
-            director: "New Director!"
-        }
-    ]);
+    const [movie, setMovie] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
+    useEffect(() => {
+        fetch("https://git.heroku.com/my-flix-app.git")
+            .then((response) => response.json())
+            .then((data) => {
+                const moviesFromApi = data.docs.map((doc) => {
+                    return {
+                        id: doc.key,
+                        title: doc.title,
+                        image: doc.image,
+                        director: doc.director_name?.[0]
+                    };
+                });
+                setMovie(moviesFromApi);
+            });
+    }, []);
 
     if (selectedMovie) {
         return (
