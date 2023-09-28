2a2637423cf49922508b9e8031a118f959c4da8c
kmerten10 <kelsey.merten@gmail.com>
2023-09-28 11:27:10 -0500

    Logins/Singup added

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,11 +1,40 @@
 import { useEffect, useState } from "react";
 import { MovieCard } from "./movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { use } from "passport";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
-    const [movie, setMovie] = useState([]);
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(null);
 
+    const [movie, setMovie] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        if (!token) return;
+        fetch("https://my-flix-app-66e818e7b7de.herokuapp.com/movies", {
+            headers: { Authorization: 'Bearer ${token' },
+        })
+            .then((response) => response.json())
+            .then((movies) => {
+                setMovies(movies);
+            });
+    }, [token]);
+
+    useEffect(() => {
+        fetch("https://my-flix-app-66e818e7b7de.herokuapp.com/movies", {
+            headers: { Authorization: 'Bearer ${token}' }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            });
+    }, { token });
+
     useEffect(() => {
         fetch("https://my-flix-app-66e818e7b7de.herokuapp.com/movies")
             .then((response) => response.json())
@@ -23,6 +52,21 @@ export const MainView = () => {
             });
     }, []);
 
+    if (!user) {
+        return (
+            <>
+                <LoginView
+                    onLoggedIn={(user, token) => {
+                        setUser(user);
+                        setToken(token);
+                    }}
+                />
+                or
+                <SignupView />
+            </>
+        );
+    }
+
     if (selectedMovie) {
         return (
             <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
@@ -35,6 +79,7 @@ export const MainView = () => {
 
     return (
         <div>
+            <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
 
             {movie.map((movie) => (
                 <MovieCard key={movie.id}
