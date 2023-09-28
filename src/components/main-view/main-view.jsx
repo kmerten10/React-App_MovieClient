ead8d1fd5422130a168f8775006f10cdc18c5f2f
kmerten10 <kelsey.merten@gmail.com>
2023-09-27 20:16:36 -0500

    reverted for exercise

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,49 +1,71 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { MovieCard } from "./movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
     const [movie, setMovie] = useState([]);
-
-    const [selectedMovie, setSelectedMovie] = useState(null);
-
-    useEffect(() => {
-        fetch("api")
-            .then((response) => response.json())
-            .then((data) => {
-                return {
-                    id: doc.key,
-                    title: doc.title,
-                    image: "https",
-                    author: doc.director_name?.[0]
-                };
-            });
-        setMovies(moviesFromApi)
-    }, []);
-
-    if (selectedMovie) {
-        return (
-            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-        );
+    {
+        id: 1,
+            title: "Mrs. Doubtfire",
+                image:
+        "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
+            director: "Someone"
+    },
+    {
+        id: 2,
+            title: "The Matrix",
+                image:
+        "https://images-na.ssl-images-amazon.com/images/I/51WAikRq37L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+            director: "Someone Else"
+    },
+    {
+        id: 3,
+            title: "Peter Rabbit",
+                image:
+        "https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX381_BO1,204,203,200_.jpg",
+            director: "Peter Rabbit Director"
+    },
+    {
+        id: 4,
+            title: "This is a Movie",
+                image:
+        "https://images-na.ssl-images-amazon.com/images/I/51HbNW6RzhL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+            director: "This is a movie director"
+    },
+    {
+        id: 5,
+            title: "New Movie!",
+                image:
+        "https://images-na.ssl-images-amazon.com/images/I/41MBLi5a4jL._SX384_BO1,204,203,200_.jpg",
+            director: "New Director!"
     }
+    ]);
 
-    if (movie.length === 0) {
-        return <div> The list is empty! </div>;
-    }
+const [selectedMovie, setSelectedMovie] = useState(null);
 
+if (selectedMovie) {
     return (
-        <div>
-
-            {movie.map((movie) => (
-                <MovieCard key={movie.id}
-                    movie={movie}
-                    onClick={() => {
-                        setSelectedMovie(movie);
-                    }}
-                />
-            ))}
-        </div>
+        <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
     );
+}
+
+if (movie.length === 0) {
+    return <div> The list is empty! </div>;
+}
+
+return (
+    <div>
+
+        {movie.map((movie) => (
+            <MovieCard key={movie.id}
+                movie={movie}
+                onClick={() => {
+                    setSelectedMovie(movie);
+                }}
+            />
+        ))}
+    </div>
+);
 };
 
 
