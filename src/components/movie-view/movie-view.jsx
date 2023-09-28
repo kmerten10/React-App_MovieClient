e2b62df769793342fc0c38fc93f3e9ee0a4276b8
kmerten10 <kelsey.merten@gmail.com>
2023-09-28 12:53:11 -0500

    updates

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -7,7 +7,7 @@ export const MovieView = ({ movie, onBackClick }) => {
             </div>
             <div>
                 <span>Director: </span>
-                <span>{movie.Director.Name}</span>
+                <span>{movie.Director}</span>
             </div>
             <button onClick={onBackClick}>Back</button>
         </div>
