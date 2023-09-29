78bbd5692c2373f451a4949710615867a93d038a
kmerten10 <kelsey.merten@gmail.com>
2023-09-29 00:36:25 -0500

    3.6

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export const SignupView = () => {
     const [username, setUsername] = useState("");
@@ -34,46 +36,54 @@ export const SignupView = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username:
-                <input
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formUsername">
+                <Form.Label>Username:</Form.Label>
+                <Form.Control
                     type="text"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                     required
                     minLength="7"
                 />
-            </label>
-            <label>
-                Password:
-                <input
+            </Form.Group>
+
+            <Form.Group controlId="formPassword">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control
                     type="text"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
-                    minLength="10"
+                    minLength="7"
                 />
-            </label>
-            <label>
-                Email:
-                <input
-                    type="email"
+            </Form.Group>
+
+            <Form.Group controlId="formEmail">
+                <Form.Label>Email:</Form.Label>
+                <Form.Control
+                    type="text"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     required
+                    minLength="7"
                 />
-            </label>
-            <label>
-                Birthday:
-                <input
-                    type="date"
+            </Form.Group>
+
+            <Form.Group controlId="formBirthday">
+                <Form.Label>Birthday:</Form.Label>
+                <Form.Control
+                    type="text"
                     value={birthday}
                     onChange={(e) => setBirthday(e.target.value)}
                     required
+                    minLength="7"
                 />
-            </label>
-            <button type="submit">Submit</button>
-        </form>
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
     );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,17 @@
 import { createRoot } from "react-dom/client";
+import Container from "react-bootstrap/Container";
 
 import { MainView } from "./components/main-view/main-view";
 
-//Import statment to indicate that you need to bundle './index.scss'
-import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./index.scss";
 
 //Main component (will eventually use all the others)
 const MyFlixApplication = () => {
-    return (<MainView />
+    return (
+        <Container>
+            <MainView />
+        </Container>
     );
 };
 
