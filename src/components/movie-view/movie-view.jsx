80aa9af1a26fe2bd0ea7956331fa0f7cef0ac889
kmerten10 <kelsey.merten@gmail.com>
2023-09-29 01:42:06 -0500

    scss changes

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -3,29 +3,32 @@ import './movie-view.scss';
 export const MovieView = ({ movie, onBackClick }) => {
     return (
         <div>
-            <div>
-                <img src={movie.Image} />
-            </div>
-            <div>
-                <span>Title: </span>
-                <span>{movie.Title}</span>
-            </div>
-            <div>
-                <span>Director: </span>
-                <span>{movie.Director.Name}</span>
-            </div>
-            <div>
-                <span>Genre: </span>
-                <span>{movie.Genre.Name}</span>
-            </div>
-            <div>
-                <span>Description: </span>
-                <span>{movie.Description}</span>
-            </div>
             <button onClick={onBackClick}
                 className="back-button"
                 style={{ cursor: "pointer" }}
             >Back</button>
+            <div className='text'>
+                <div>
+                    <span>{movie.Title}</span>
+                </div>
+
+                <div>
+                    <img src={movie.Image} />
+                </div>
+
+                <div>
+
+                    <span>{movie.Genre.Name}</span>
+                </div>
+                <div>
+                    <span>{movie.Description}</span>
+                </div>
+                <div>
+                    <span>Director: </span>
+                    <span>{movie.Director.Name}</span>
+                </div>
+
+            </div>
         </div>
     );
 };
