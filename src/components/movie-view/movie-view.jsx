87ff34f415499aff0440ac21a3dd80fb4f437851
kmerten10 <kelsey.merten@gmail.com>
2023-10-02 10:50:05 -0500

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,8 +1,22 @@
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import './movie-view.scss';
 
 export const MovieView = ({ movie, onBackClick }) => {
+    const { movieId } = useParams();
+
+    const movies = movies.find((b) => b.id === movieId);
+
     return (
         <div>
+
+            <Link to={`/movies`}>
+                <button onClick={onBackClick}
+                    className="back-button"
+                    style={{ cursor: "pointer" }}
+                >Back</button>
+            </Link>
+
             <div className='text'>
                 <div>
                     <span>{movie.Title}</span>
@@ -25,10 +39,6 @@ export const MovieView = ({ movie, onBackClick }) => {
                 </div>
 
             </div>
-            <button onClick={onBackClick}
-                className="back-button"
-                style={{ cursor: "pointer" }}
-            >Back</button>
         </div>
     );
 };
